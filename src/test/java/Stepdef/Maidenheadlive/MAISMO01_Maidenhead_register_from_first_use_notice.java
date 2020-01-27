e571d0ce7ce6c17c48055887d01747d9bfa21671
Agateone <jayasree.kamineni1@gmail.com>
2020-01-27 12:38:52 +0000

    added sleep statements

--- src/test/java/Stepdef/Maidenheadlive/MAISMO01_Maidenhead_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Maidenheadlive/MAISMO01_Maidenhead_register_from_first_use_notice.java
@@ -32,7 +32,7 @@ public class MAISMO01_Maidenhead_register_from_first_use_notice {
 		
 				//chrome
 		if (browser.equalsIgnoreCase("chrome")) { 
-			System.setProperty("webdriver.chrome.driver","/Users/jay/eclipse-workspace/chromedriver");				
+			System.setProperty("webdriver.chrome.driver","C:/Users/Administrator/Desktop/chromedriver.exe");				
 			driver= new ChromeDriver();				
 			driver.get("https://www.maidenhead-advertiser.co.uk/news/remember-when/152676/remember-when-neighbours-star-draws-a-crowd-and-protests-over-m4-widening.html");
 			driver.findElement(By.linkText("Ok")).click();
