0627d33ff0b2631b22cb8e529b4901a8065a954b
Agateone <jayasree.kamineni1@gmail.com>
2020-02-05 11:13:03 +0000

    change sleep to 10seconds after

--- src/test/java/Stepdef/Maidenheadlive/MAISMO01_Maidenhead_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Maidenheadlive/MAISMO01_Maidenhead_register_from_first_use_notice.java
@@ -34,8 +34,9 @@ public class MAISMO01_Maidenhead_register_from_first_use_notice {
 			//driver.findElement(By.linkText("Ok")).click();
 			Thread.sleep(10000);
 			Popbitch_First_Use_Notice_Elements popbitch_first_use_elements= new Popbitch_First_Use_Notice_Elements(driver);
+			Thread.sleep(6000);
 			popbitch_first_use_elements.Click_On_Popbitch_First_Use_Notice_Create_Wallet();
-		    
+		    Thread.sleep(20000);
 			String reg_Page_url= driver.getCurrentUrl();
 			System.out.println(reg_Page_url);
 			if(reg_Page_url.contains("https://account.agate.io/my-agate/sign-up?"))
